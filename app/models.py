bdfe240d10982529de47e1d7c4da00d1d3943fbe
selfsryo <selfsryo@gmail.com>
2020-07-23 16:40:20 +0900

    models.py修正

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from markdownx.models import MarkdownxField
 from markdownx.utils import markdownify
+from django.utils.safestring import mark_safe
 
 class Post(models.Model):
 	title = models.CharField('タイトル', max_length=255)
 	main_text = MarkdownxField('本文')
 
 	def convert_markdown_to_html(self):
-		return markdownify(self.main_text)
+		return mark_safe(markdownify(self.main_text))
